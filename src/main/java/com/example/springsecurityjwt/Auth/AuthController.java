04bf42819de3dbab264613aa7ab29dd53b9fa32b
LucaZamperoni <luca.zamperoniz@gmail.com>
2023-11-10 20:39:43 -0300

    Adding JWT.

--- src/main/java/com/example/springsecurityjwt/Auth/AuthController.java
+++ src/main/java/com/example/springsecurityjwt/Auth/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.springsecurityjwt.Auth;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
     @PostMapping(value = "login")
-    public String login() {
-        return "Login from public Endpoint";
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
     }
 
     @PostMapping(value = "register")
-    public String register() {
-        return "Register from public Endpoint";
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
     }
 }
